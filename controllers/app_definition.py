22c9ac009e664c784da630a23b094d1eff74f410
jesus oliveros <jesuso@microsoft.com>
2022-07-03 13:38:00 -0500

    update new queries

--- controllers/app_definition.py
+++ controllers/app_definition.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request, jsonify
 from flask_cors import CORS, cross_origin
 import os
-from .db import executeQuery 
+from .db import executeQuery, tablaAgente
 from configparser import SafeConfigParser
 from openpyxl import Workbook
 
@@ -23,15 +23,23 @@ def create_app(test_config=None):
     except OSError:
         pass
 
-    @app.route('/iagent', methods= ['GET'])
+    @app.route('/agent/<correo>', methods= ['GET'])
     @cross_origin(origin='0.0.0.0',headers=['Content- Type','Authorization'])
-    def get_data():
+    def get_data(correo):
         #rows = executeQuery("PRAGMA database_list;")
-        rows = executeQuery("SELECT * FROM agente")
+        rows = executeQuery("SELECT * FROM agente where correo == \""+correo+"\"")
         data =[]
         for row in rows:
             data.append([x for x in row])
         return jsonify(data)
+
+    @app.route('/agent', methods= ['POST'])
+    @cross_origin(origin='0.0.0.0',headers=['Content- Type','Authorization'])
+    def insertAgent():
+        jsonValue = request.get_json()
+        tablaAgente('INSERTAR',jsonValue.get('name'),jsonValue.get('address'),jsonValue.get('phone'),jsonValue.get('email'))
+        return 'ok'
+
 
 
     @app.route('/checking', methods= ['GET'])
